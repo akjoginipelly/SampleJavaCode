b321fa29c631b3fbbedb1609aa7fa39b446f30c9
Abhinav Joginipelly <abhinavjoginipelly@gmail.com>
2021-08-08 17:13:56 -0500

    Initial Commit

---
+++ src/student/Addition.java
@@ -0,0 +1,14 @@
+package student;
+
+public class Addition {
+
+    private static   int a = 10;
+    private static   int b = 20;
+
+   private static int c = a + b;
+
+    public static void main(String[] args) {
+
+        System.out.println("Addition of 2 numbers: " +c);
+    }
+}
---
+++ src/student/Student.java
@@ -0,0 +1,19 @@
+package student;
+
+public class Student {
+
+    private static String student1 = "Hello Abhinav";
+   private static String student2 = "Welcome to Java";
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+        System.out.println("Abhinav");
+        System.out.println("Welcome to Java");
+
+        System.out.println("*************************************");
+
+        System.out.println(student1 + " " + student2);
+        System.out.println(student2);
+    }
+}
